4dbd022c6d844e56654a2408370d39fab83a79a5
rizkaasta <rizkaasta98@gmail.com>
2022-03-17 19:45:01 +0700

    main

--- main.go
+++ main.go
@@ -1,3 +1,11 @@
 package main
 
-import 
+import "github.com/gin-gonic/gin"
+import handler "github.com/rizkaasta/tugas_handler"
+
+func main() {
+	r := gin.Default()
+	r.GET("/item", handler.Product)
+	r.GET("/id/:id/produk", handler.IdProduct)
+	r.Run()
+}
